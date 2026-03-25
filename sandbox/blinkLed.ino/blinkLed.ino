ecfb175921ec11670e5a81a60fb093c0b47c7d57
yureru <bryan.risc@gmail.com>
2026-03-25 00:33:41 -0600

    led blink example

---
+++ sandbox/blinkLed.ino/blinkLed.ino
@@ -0,0 +1,14 @@
+#define PIN_12 12
+
+void setup() {
+}
+
+void loop() {
+
+  // turn on and off the led
+  digitalWrite(PIN_12, HIGH);
+  delay(500);
+
+  digitalWrite(PIN_12, LOW);
+  delay(500);
+}
